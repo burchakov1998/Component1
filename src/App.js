4918865b28893ada4a4ed1f6fc7dbda337820dea
burchakov1998 <124458140+burchakov1998@users.noreply.github.com>
2023-08-21 15:24:32 +0300

    modifed

--- src/App.js
+++ src/App.js
@@ -10,17 +10,17 @@ const item = {
 }
 function ShopItemFunc (){
   return (
-<div class="main-content">
+<div className="main-content">
   <h2>Tiger of Sweden</h2>
   <h1>Leonard coat</h1>
   <h3>Minimalistic coat in cotton-blend</h3>
   <div class="description">
     Men's minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.
   </div>
-  <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
-  <div class="divider"></div>
-  <div class="purchase-info">
-    <div class="price">£405.00</div>
+  <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+  <div className="divider"></div>
+  <div className="purchase-info">
+    <div className="price">£405.00</div>
     <button>Добавить в корзину</button>
   </div>
 </div>
@@ -41,20 +41,7 @@ function App() {
            <ShopItemFunc item={item} />
          </div>
        </div>
-      /* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */
+  
       }
     </div>
   );
