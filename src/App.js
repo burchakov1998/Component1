a68804161dfac8fb356ddde11ba9dfc94707392a
burchakov1998 <124458140+burchakov1998@users.noreply.github.com>
2023-08-21 15:02:38 +0300

    modifed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+const item = {
+  brand: 'Tiger of Sweden',
+  title: 'Leonard coat',
+  description: 'Minimalistic coat in cotton-blend',
+  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+  price: 399,
+  currency: '£'
+}
+function ShopItemFunc (){
+  return (
+<div class="main-content">
+  <h2>Tiger of Sweden</h2>
+  <h1>Leonard coat</h1>
+  <h3>Minimalistic coat in cotton-blend</h3>
+  <div class="description">
+    Men's minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.
+  </div>
+  <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
+  <div class="divider"></div>
+  <div class="purchase-info">
+    <div class="price">£405.00</div>
+    <button>Добавить в корзину</button>
+  </div>
+</div>
+  );
 
+} 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {
+         <div className="container">
+         <div className="background-element">
+         </div>
+         <div className="highlight-window">
+           <div className='highlight-overlay'></div>
+         </div>
+         <div className="window">
+           <ShopItemFunc item={item} />
+         </div>
+       </div>
+      /* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +54,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */
+      }
     </div>
   );
 }
